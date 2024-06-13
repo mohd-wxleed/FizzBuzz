b3785e1301fd250e64b757cdfd9ab211022d1719
mohd-wxleed <mohstrong2005@gmail.com>
2024-06-13 07:15:48 -0400

    done with task2

---
+++ src/Reduce.java
@@ -0,0 +1,17 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int steps = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n = n/2;
+                steps += 1;
+            } else if (n % 2 != 0) {
+                n = n - 1;
+                steps += 1;
+            }
+
+        }
+        System.out.println(steps);
+    }
+}
